78fdb12e31ac8045a831903062cdcba9e499d1d0
AshTrickz <ashshvern@gmail.com>
2024-05-07 20:53:18 +0300

    Multiple Room Generation Base

--- Assets/Scripts/DungeonGeneration/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGeneration/DungeonGenerator.cs
@@ -19,7 +19,7 @@ public class DungeonGenerator : SerializedMonoBehaviour
     [HorizontalGroup("Tilemaps", .5f), LabelWidth(80), SerializeField]
     private Tilemap floorTilemap, wallTilemap;
 
-    [Title("Generation Data", titleAlignment: TitleAlignments.Centered), MinMaxSlider(3, 10), LabelWidth(145f),
+    [Title("Generation Data", titleAlignment: TitleAlignments.Centered), MinMaxSlider(2, 10), LabelWidth(145f),
      SerializeField, SuffixLabel("$_roomsCountString")]
     private Vector2Int roomCount = new(5, 7);
 
@@ -47,49 +47,87 @@ public class DungeonGenerator : SerializedMonoBehaviour
     private string _doorwaysCountString => $"Min = {doorwaysPerRoom.x}, Max = {doorwaysPerRoom.y}";
 #endif
 
+    private int _roomsLeftToGenerate = 0;
+
     [Button, HorizontalGroup("Generate Dungeon", PaddingLeft = 15f)]
     public void GenerateDungeon()
     {
         ClearDungeon();
-        // for (int i = 0; i < roomCount; i++)
-        // {
-        var roomWidth = Random.Range(minimumWidth, maximumWidth);
-        var roomHeight = Random.Range(minimumHeight, maximumHeight);
 
-        var doorwaysAmount = Random.Range(doorwaysPerRoom.x, doorwaysPerRoom.y + 1);
+        _roomsLeftToGenerate = Random.Range(roomCount.x, roomCount.y + 1);
+
+        GenerateRoom(DoorwayDirection.ORIGIN, Vector2Int.zero);
+    }
+
+    private void GenerateRoom(DoorwayDirection doorwayKey, Vector2Int doorwayTilePosition)
+    {
+        var roomWidth = Random.Range(minimumWidth, maximumWidth + 1);
+        var roomHeight = Random.Range(minimumHeight, maximumHeight + 1);
 
         if (roomWidth % 2 == 0) roomWidth += 1;
         if (roomHeight % 2 == 0) roomHeight += 1;
 
-        Vector2Int spawnPosition = new Vector2Int(0, 0);
+        var doorwaysAmount = Random.Range(doorwaysPerRoom.x, doorwaysPerRoom.y + 1);
+        Dictionary<DoorwayDirection, Vector2Int> roomDoorways = new();
+
+        Vector2Int spawnPosition = Vector2Int.zero;
+        switch (doorwayKey)
+        {
+            case DoorwayDirection.Left:
+                spawnPosition = new Vector2Int(doorwayTilePosition.x - roomWidth - 1,
+                    doorwayTilePosition.y - roomHeight / 2);
+                break;
+            case DoorwayDirection.Bottom:
+                spawnPosition = new Vector2Int(doorwayTilePosition.x - roomWidth / 2, doorwayTilePosition.y - roomHeight - 2);
+                break;
+            case DoorwayDirection.Right:
+                spawnPosition = new Vector2Int(doorwayTilePosition.x + 2, doorwayTilePosition.y - roomHeight / 2);
+                break;
+            case DoorwayDirection.Top:
+                spawnPosition = new Vector2Int(doorwayTilePosition.x - roomWidth / 2, doorwayTilePosition.y + 2);
+                break;
+            case DoorwayDirection.ORIGIN:
+                break;
+        }
+
+        Debug.Log(
+            $"Rooms Spawn Point {spawnPosition}, Width: {roomWidth}, Height: {roomHeight}, Doorway Tile Position: {doorwayTilePosition}");
 
         HashSet<Vector2Int> floor = GenerateFloor(spawnPosition, roomWidth, roomHeight);
-        HashSet<Vector2Int> walls = GenerateWalls(floor, roomWidth, roomHeight, doorwaysAmount);
+        HashSet<Vector2Int> walls = GenerateWalls(floor, roomWidth, roomHeight, doorwaysAmount, out roomDoorways);
 
+        Debug.Log(floor.Count);
+        
         PaintTiles(floor, floorTilemap, tilingPreset.FloorTile);
+        //List<Prop> props = GenerateProps();
 
-        //AddRoomData(floor, walls);
-
-        //}
+        //AddRoomData(floor, walls, props);
+        _roomsLeftToGenerate--;
+        if (_roomsLeftToGenerate <= 0) return;
+        // POSSIBLE RECURSION!!!
+        foreach (var doorway in roomDoorways)
+        {
+            GenerateRoom(doorway.Key, doorway.Value); // Possible do neighbours
+        }
     }
 
     private HashSet<Vector2Int> GenerateFloor(Vector2Int cornerSpawnPosition, int roomWidth, int roomHeight)
     {
         HashSet<Vector2Int> floor = new();
 
-        for (int i = cornerSpawnPosition.x; i < roomWidth; i++)
+        for (int i = cornerSpawnPosition.x; i < cornerSpawnPosition.x + roomWidth; i++)
         {
-            for (int j = cornerSpawnPosition.y; j < roomHeight; j++)
+            for (int j = cornerSpawnPosition.y; j < cornerSpawnPosition.y + roomHeight; j++)
             {
                 floor.Add(new Vector2Int(i, j));
             }
         }
-        
+
         return floor;
     }
 
     private HashSet<Vector2Int> GenerateWalls(HashSet<Vector2Int> floor, int roomWidth, int roomHeight,
-        int doorwaysAmount)
+        int doorwaysAmount, out Dictionary<DoorwayDirection, Vector2Int> roomDoorways)
     {
         HashSet<Vector2Int> walls = new();
 
@@ -162,30 +200,34 @@ public class DungeonGenerator : SerializedMonoBehaviour
         PaintSingleTile(wallTilemap, tilingPreset.WallTilesDictionary[tilingPreset.LeftKey], leftUpperCorner);
         PaintSingleTile(wallTilemap, tilingPreset.WallTilesDictionary[tilingPreset.LeftUpKey], leftUpperUpperCorner);
 
-        GenerateDoorways(minX, maxX, minY, maxY, roomWidth, roomHeight, doorwaysAmount, ref floor, ref walls);
+        GenerateDoorways(minX, maxX, minY, maxY, roomWidth, roomHeight, doorwaysAmount, floor,
+            out roomDoorways);
 
         return walls;
     }
 
     private void GenerateDoorways(int minX, int maxX, int minY, int maxY, int roomWidth, int roomHeight,
-        int doorwaysAmount, ref HashSet<Vector2Int> floor, ref HashSet<Vector2Int> walls)
+        int doorwaysAmount, HashSet<Vector2Int> floor, out Dictionary<DoorwayDirection, Vector2Int> roomDoorways)
     {
-        List<DoorwayDirection> doorwayDirections = RandomizeDoorways(doorwaysAmount);
+        List<DoorwayDirection> directionsList = RandomizeDoorways(doorwaysAmount);
+        Dictionary<DoorwayDirection, Vector2Int> doorwayDirections = new();
 
-        if (doorwayDirections.Contains(DoorwayDirection.Left))
+        if (directionsList.Contains(DoorwayDirection.Left))
         {
             var leftDoorWayUp = new Vector2Int(minX - 1, minY + roomHeight / 2 + 1);
             var leftDoorWayUpUp = new Vector2Int(leftDoorWayUp.x, leftDoorWayUp.y + 1);
             var leftDoorWayDown = new Vector2Int(minX - 1, minY + roomHeight / 2 - 1);
 
-            Vector2Int doorwayTilePosition = new Vector2Int(minX - 1, roomHeight / 2);
+            Vector2Int doorwayTilePosition = new Vector2Int(minX - 1, minY + roomHeight / 2);
 
-            walls.Add(leftDoorWayUp);
-            walls.Add(leftDoorWayUpUp);
-            walls.Add(leftDoorWayDown);
-
-            walls.Remove(doorwayTilePosition);
+            // walls.Add(leftDoorWayUp);
+            // walls.Add(leftDoorWayUpUp);
+            // walls.Add(leftDoorWayDown);
+            //
+            // walls.Remove(doorwayTilePosition);
             floor.Add(doorwayTilePosition);
+
+            doorwayDirections.Add(DoorwayDirection.Left, doorwayTilePosition);
 
             PaintSingleTile(wallTilemap, null, doorwayTilePosition);
 
@@ -196,18 +238,20 @@ public class DungeonGenerator : SerializedMonoBehaviour
                 leftDoorWayDown);
         }
 
-        if (doorwayDirections.Contains(DoorwayDirection.Bottom))
+        if (directionsList.Contains(DoorwayDirection.Bottom))
         {
             var bottomLeftDoorWay = new Vector2Int(minX + roomWidth / 2 - 1, minY - 1);
             var bottomRightDoorWay = new Vector2Int(minX + roomWidth / 2 + 1, minY - 1);
 
-            Vector2Int doorwayTilePosition = new Vector2Int(roomWidth / 2, minY - 1);
+            Vector2Int doorwayTilePosition = new Vector2Int(minX + roomWidth / 2, minY - 1);
 
-            walls.Add(bottomLeftDoorWay);
-            walls.Add(bottomRightDoorWay);
-
-            walls.Remove(doorwayTilePosition);
+            // walls.Add(bottomLeftDoorWay);
+            // walls.Add(bottomRightDoorWay);
+            //
+            // walls.Remove(doorwayTilePosition);
             floor.Add(doorwayTilePosition);
+
+            doorwayDirections.Add(DoorwayDirection.Bottom, doorwayTilePosition);
 
             PaintSingleTile(wallTilemap, null, doorwayTilePosition);
 
@@ -217,20 +261,22 @@ public class DungeonGenerator : SerializedMonoBehaviour
                 bottomRightDoorWay);
         }
 
-        if (doorwayDirections.Contains(DoorwayDirection.Right))
+        if (directionsList.Contains(DoorwayDirection.Right))
         {
             var rightDoorWayUp = new Vector2Int(maxX + 1, minY + roomHeight / 2 + 1);
             var rightDoorWayUpUp = new Vector2Int(rightDoorWayUp.x, rightDoorWayUp.y + 1);
             var rightDoorWayDown = new Vector2Int(maxX + 1, minY + roomHeight / 2 - 1);
 
-            Vector2Int doorwayTilePosition = new Vector2Int(maxX + 1, roomHeight / 2);
+            Vector2Int doorwayTilePosition = new Vector2Int(maxX + 1, minY + roomHeight / 2);
 
-            walls.Add(rightDoorWayUp);
-            walls.Add(rightDoorWayUpUp);
-            walls.Add(rightDoorWayDown);
-
-            walls.Remove(doorwayTilePosition);
+            // walls.Add(rightDoorWayUp);
+            // walls.Add(rightDoorWayUpUp);
+            // walls.Add(rightDoorWayDown);
+            //
+            // walls.Remove(doorwayTilePosition);
             floor.Add(doorwayTilePosition);
+
+            doorwayDirections.Add(DoorwayDirection.Right, doorwayTilePosition);
 
             PaintSingleTile(wallTilemap, null, doorwayTilePosition);
 
@@ -241,7 +287,7 @@ public class DungeonGenerator : SerializedMonoBehaviour
                 rightDoorWayDown);
         }
 
-        if (doorwayDirections.Contains(DoorwayDirection.Top))
+        if (directionsList.Contains(DoorwayDirection.Top))
         {
             var topLeftDoorWay = new Vector2Int(minX + roomWidth / 2 - 1, maxY + 1);
             var topRightDoorWay = new Vector2Int(minX + roomWidth / 2 + 1, maxY + 1);
@@ -249,18 +295,20 @@ public class DungeonGenerator : SerializedMonoBehaviour
             var topLeftDoorWayUp = new Vector2Int(topLeftDoorWay.x, topRightDoorWay.y + 1);
             var topRightDoorWayUp = new Vector2Int(topRightDoorWay.x, topRightDoorWay.y + 1);
 
-            Vector2Int doorwayTilePosition1 = new Vector2Int(roomWidth / 2, maxY + 1);
-            Vector2Int doorwayTilePosition2 = new Vector2Int(roomWidth / 2, maxY + 2);
+            Vector2Int doorwayTilePosition1 = new Vector2Int(minX + roomWidth / 2, maxY + 1);
+            Vector2Int doorwayTilePosition2 = new Vector2Int(minX + roomWidth / 2, maxY + 2);
 
-            walls.Add(topLeftDoorWay);
-            walls.Add(topRightDoorWay);
-            walls.Add(topLeftDoorWayUp);
-            walls.Add(topRightDoorWayUp);
-
-            walls.Remove(doorwayTilePosition1);
-            walls.Remove(doorwayTilePosition2);
+            // walls.Add(topLeftDoorWay);
+            // walls.Add(topRightDoorWay);
+            // walls.Add(topLeftDoorWayUp);
+            // walls.Add(topRightDoorWayUp);
+            //
+            // walls.Remove(doorwayTilePosition1);
+            // walls.Remove(doorwayTilePosition2);
             floor.Add(doorwayTilePosition1);
             floor.Add(doorwayTilePosition2);
+
+            doorwayDirections.Add(DoorwayDirection.Top, doorwayTilePosition2);
 
             PaintSingleTile(wallTilemap, null, doorwayTilePosition1);
             PaintSingleTile(wallTilemap, null, doorwayTilePosition2);
@@ -274,6 +322,10 @@ public class DungeonGenerator : SerializedMonoBehaviour
             PaintSingleTile(wallTilemap, tilingPreset.WallTilesDictionary[tilingPreset.RightDoorWayUpUp],
                 topRightDoorWayUp);
         }
+
+        roomDoorways = new();
+        foreach (var dir in doorwayDirections)
+            roomDoorways.Add(dir.Key, dir.Value);
     }
 
     private List<DoorwayDirection> RandomizeDoorways(int doorwaysAmount)
@@ -335,6 +387,7 @@ public class DungeonGenerator : SerializedMonoBehaviour
         Left,
         Bottom,
         Right,
-        Top
+        Top,
+        ORIGIN
     }
 }
